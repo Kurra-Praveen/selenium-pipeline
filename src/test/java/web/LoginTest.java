0ae9d0d3bf9a586b4f4e0af1b4127b05aabca239
praveen <praveenkurra123@gmail.com>
2025-03-28 00:36:03 +0530

    Modified the process

--- src/test/java/web/LoginTest.java
+++ src/test/java/web/LoginTest.java
@@ -4,19 +4,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
+
 
 public class LoginTest {
 
 	@Test
 	public void LoginApp(){
 		WebDriverManager.chromedriver().setup();
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // Required for GitHub Actions
+		options.addArguments("--no-sandbox"); // Avoid sandboxing issues on Linux
+		options.addArguments("--disable-dev-shm-usage"); // Prevent shared memory issues
+		options.addArguments("--disable-gpu"); // Disable GPU in headless mode
+		options.addArguments("--remote-allow-origins=*"); // Fix WebSocket issues in newer Selenium
+		// Explicitly set a temporary user data directory (optional, but resolves the error)
+		options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.currentTimeMillis());
 
 		WebDriver driver=new ChromeDriver();
 
